916656fa842406e9b7e8dcb2f8556b2a260c18a6
AhmedKamalSaad <AhmedKamalSaad110@gmail.com>
2025-06-11 18:32:15 +0300

    force dynamic force no store

--- app/suppliers/page.tsx
+++ app/suppliers/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 import { PartyType } from "@prisma/client";
 import { PartiesTable } from "../_components/PartiesTable";
 import { getPartiesWithSummary } from "../lib/parties";
